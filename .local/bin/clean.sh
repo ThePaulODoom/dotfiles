73e822fe4446e77022ffa0ff907cb695f8875b0a
Your Name <you@example.com>
2020-05-16 17:55:49 -0500

    automcomplete vim

---
+++ .local/bin/clean.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# Rust cargo
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+
+# ZSH
+export ZDOTDIR="$HOME/.config/zsh"
+
+# Xfiles
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xfiles"
+export XINITRC ="$XDG_RUNTIME_DIR/Xfiles"
+export XSERVERRC ="$XDG_RUNTIME_DIR/Xfiles"
+
+# GnuPG
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+gpg2 --homedir "$XDG_DATA_HOME"/gnupg
+
+# KDE
+export KDEHOME="$XDG_CONFIG_HOME"/kde
+
+# Less
+export LESSHISTFILE=-
+
+# Parallel
+export PARALLEL_HOME="$XDG_CONFIG_HOME"/parallel
+
+# SVN
+svn --config-dir "$XDG_CONFIG_HOME"/subversion
