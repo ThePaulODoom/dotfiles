227dc0ee93bc7cac5089d29d73685fe4f83abb05
ThePaulODoom <thepaulodoom@thepaulodoom.com>
2021-11-27 21:39:25 -0600

    fixed status bar and added mutt config

--- .config/dwm/status.sh
+++ .config/dwm/status.sh
@@ -3,10 +3,10 @@ timeanddate() {
 	date +"%A, %B %e %T %Z"
 }
 bat() {
-	echo -e "$(cat /sys/class/power_supply/BAT1/capacity) ($(cat /sys/class/power_supply/BAT1/capacity_level))"
+	echo "$(cat /sys/class/power_supply/BAT1/capacity) ($(cat /sys/class/power_supply/BAT1/capacity_level))"
 }
 
 while true; do
-	xsetroot -name "$(echo -e "$(bat) | $(timeanddate)")"
+	xsetroot -name "$(echo "$(bat) | $(timeanddate)")"
 	sleep 1s;
 done
